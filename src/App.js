929e77121157824e06ffa3f6bcb3239652fb5dd9
kevinalbert7 <20ko@live.fr>
2021-11-16 10:53:14 +0100

    oovice3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 
-import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link} from 'react-router-dom'
 
+import Home from './pages/Home'
 import Weekly from './pages/Weekly'
 import WeeklyBattle from './pages/WeeklyBattle'
 import Popular from './pages/Popular'
 import PopularBattle from './pages/PopularBattle'
 import Favorites from './pages/Favorites'
 import Error404 from './pages/Error404'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
+    // console.log(BrowserRouter)
     return (
-        <div>
-          <h1>Home</h1>
-        </div>
+      <BrowserRouter>
+      <div className="row m-3">
+          <nav>
+              <ul>
+                  <li>
+                    <li><Link to="/Home">home</Link></li>
+                    <li><Link to="/weekly">weekly</Link></li>
+                    <li><Link to="/WeeklyBattle">weeklyBattle</Link></li>
+                    <li><Link to="/Popular">popular</Link></li>
+                    <li><Link to="/PopularBattle">popularBattle</Link></li>
+                    <li><Link to="/Favorites">favorites</Link></li>
+                    <li><Link to="/Error404">Error404</Link></li>
+                  </li>
+              </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/weekly" component={Weekly}/>
+            <Route path="/WeeklyBattle" component={WeeklyBattle}/>
+            <Route path="Popular" component={Popular}/>
+            <Route path="PopularBattle" component={PopularBattle}/>
+            <Route path="/Favorites" component={Favorites}/>
+            <Route path="*" component={Error404}/>
+          </Switch>
+      </div>
+  </BrowserRouter>
     );
   }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,36 +1,13 @@
 import React, { Component } from 'react';
 
-import { Link } from 'react-router-dom'
-
 class Home extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <div>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/Weekly">weekly</Link>
-                                <Link to="/WeeklyBattle">weeklyBattle</Link>
-                                <Link to="/Popular">popular</Link>
-                                <Link to="/PopularBattle">popularBattle</Link>
-                                <Link to="/Favorites">favorites</Link>
-                                <Link to="/Error404">Error404</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                    <Switch>
-                        <Route path="Weekly" component={weekly}/>
-                        <Route path="WeeklyBattle" component={weeklyBattle}/>
-                        <Route path="Popular" component={popular}/>
-                        <Route path="PopularBattle" component={popularBattle}/>
-                        <Route path="Favorites" component={favorites}/>
-                        <Route path="*" component={Error404}/>
-                    </Switch>
-                </div>
-            </BrowserRouter>
+            <div>
+                <h1>Home</h1>
+            </div>
         );
     }
 }
 
-export default home;
+export default Home;
